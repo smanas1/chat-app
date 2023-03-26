b9e34d0d87a9d61649826c4d96e6001ccf2fac63
anas69-cry <anasolin01777@gmail.com>
2023-03-26 23:49:49 +0600

    friend add and remove complete

--- src/components/FriendRequest/FriendRequest.js
+++ src/components/FriendRequest/FriendRequest.js
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./friendrequest.css";
 import Button from "@mui/material/Button";
 import "../Style/home-page.css";
+import {
+  getDatabase,
+  ref,
+  onValue,
+  set,
+  push,
+  remove,
+} from "firebase/database";
+import { useSelector } from "react-redux";
 
 const FriendRequest = () => {
-  let list = (
-    <div className="home-items-wrapper">
-      <div className="home-items-img friend-req-color"> </div>
-      <div className="home-items-title friend-req-title">
-        <h4>Anas</h4>
-        <p>this will next</p>
-      </div>
-      <div className="home-items-btn friend-req-btn">
-        <Button variant="contained" size="small">
-          Accept
-        </Button>
-        <Button className="friend-req-btn-2 " variant="contained" size="small">
-          Reject
-        </Button>
-      </div>
-    </div>
-  );
+  const [frndreq, setFriendreq] = useState([]);
+  const db = getDatabase();
+  const user = useSelector((users) => users.login.loggedin);
+  //friend req show
+  useEffect(() => {
+    const starCountRef = ref(db, "FriendReq/");
+    onValue(starCountRef, (snapshot) => {
+      let frndarr = [];
+      snapshot.forEach((item) => {
+        if (item.val().reciverId == user.uid) {
+          frndarr.push({ ...item.val(), id: item.key });
+        }
+      });
+      setFriendreq(frndarr);
+    });
+  }, []);
+  // accept handle
+  const handleAccept = (data) => {
+    set(push(ref(db, "Friend/")), {
+      ...data,
+    }).then(() => {
+      remove(ref(db, "FriendReq/" + data.id));
+    });
+  };
   return (
     <>
       <div className="grouplist home-item friend-req-mt">
@@ -28,16 +44,31 @@ const FriendRequest = () => {
           <h4>Friend Request</h4>
         </div>
         <div className="scroll friend-req-scroll">
-          {list}
-          {list}
-          {list}
-          {list}
-          {list}
-          {list}
-          {list}
-          {list}
-          {list}
-          {list}
+          {frndreq.map((item, i) => (
+            <div className="home-items-wrapper" key={i}>
+              <div className="home-items-img friend-req-color"> </div>
+              <div className="home-items-title friend-req-title">
+                <h4>{item.sendername}</h4>
+                <p>this will next</p>
+              </div>
+              <div className="home-items-btn friend-req-btn">
+                <Button
+                  variant="contained"
+                  size="small"
+                  onClick={() => handleAccept(item)}
+                >
+                  Accept
+                </Button>
+                <Button
+                  className="friend-req-btn-2 "
+                  variant="contained"
+                  size="small"
+                >
+                  Reject
+                </Button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
--- src/components/UserList/Userlist.js
+++ src/components/UserList/Userlist.js
@@ -4,22 +4,61 @@ import "../UserList/userlist.css";
 import Button from "@mui/material/Button";
 import { MdGroups } from "react-icons/md";
 
-import { getDatabase, ref, onValue } from "firebase/database";
+import {
+  getDatabase,
+  ref,
+  onValue,
+  set,
+  push,
+  remove,
+} from "firebase/database";
+import { useSelector } from "react-redux";
 
 const Userlist = () => {
+  const user = useSelector((users) => users.login.loggedin);
   const [userarrs, setUserarrs] = useState([]);
+  const [frndreq, setFriendreq] = useState([]);
   const db = getDatabase();
   useEffect(() => {
     const starCountRef = ref(db, "users/");
     onValue(starCountRef, (snapshot) => {
       let userarr = [];
       snapshot.forEach((userlist) => {
-        userarr.push(userlist.val());
+        if (user.uid != userlist.key) {
+          userarr.push({ ...userlist.val(), id: userlist.key });
+        }
       });
       setUserarrs(userarr);
     });
   }, []);
-  console.log(userarrs);
+
+  const handlesendreq = (item) => {
+    set(push(ref(db, "FriendReq/")), {
+      sendername: user.displayName,
+      senderId: user.uid,
+      reciverId: item.id,
+      recivername: item.username,
+    });
+  };
+  //friend req cancel or add
+  useEffect(() => {
+    const starCountRef = ref(db, "FriendReq/");
+    onValue(starCountRef, (snapshot) => {
+      let frndarr = [];
+      snapshot.forEach((item) => {
+        frndarr.push(item.val().reciverId + item.val().senderId);
+        frndarr.push({ ...item, id: item.key });
+      });
+      setFriendreq(frndarr);
+    });
+  }, []);
+
+  // friend req delete
+  const handleCanclereq = (data) => {
+    remove(ref(db, "FriendReq/" + frndreq[1].id));
+    // console.log(frndreq[1].id)
+  };
+  console.log();
   return (
     <>
       <div className="grouplist home-item userlist">
@@ -27,7 +66,7 @@ const Userlist = () => {
           <h4>Users List</h4>
         </div>
         <div className="scroll userlist-scrool">
-          {userarrs.map((item, i) => {
+          {userarrs.map((item, i, key) => {
             return (
               <div key={i} className="home-items-wrapper">
                 <div className="home-items-img mygroup-item-img">
@@ -41,9 +80,24 @@ const Userlist = () => {
                   <p>this will next</p>
                 </div>
                 <div className="home-items-btn">
-                  <Button variant="contained" size="small">
-                    Add Friend
-                  </Button>
+                  {frndreq.includes(user.uid + item.id) ||
+                  frndreq.includes(item.id + user.uid) ? (
+                    <Button
+                      variant="contained"
+                      size="small"
+                      onClick={() => handleCanclereq(item)}
+                    >
+                      cancel
+                    </Button>
+                  ) : (
+                    <Button
+                      variant="contained"
+                      size="small"
+                      onClick={() => handlesendreq(item)}
+                    >
+                      Add
+                    </Button>
+                  )}
                 </div>
               </div>
             );
